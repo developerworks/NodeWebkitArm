e43105c6541c71a9fc33e5771ccd14744319c3e7
Sean Nicholls <seannicholls@gmail.com>
2013-03-11 13:40:19 +0000

    added scripts to package build output
    Signed-off-by: Sean Nicholls <seannicholls@gmail.com>

---
+++ package-nodewebkitarm.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+set -o nounset
+set -o errexit
+
+if [ ! -n "${NODEWEBKITARM-}" ]; then
+        echo "Environment is not correctly configured!"
+        exit 1
+fi
+
+OUTPUT_PATH="${NODEWEBKITARM}/src/out/Release"
+BUILD_TARGET="nw"
+PACKAGE_PATH="${BUILD_TARGET}-package"
+PACKAGE_FILE="${BUILD_TARGET}-$(date +%Y%m%d).tar.gz"
+
+clean() {
+	cd ${OUTPUT_PATH}
+
+	if [ -d "${PACKAGE_PATH}" ]; then
+                rm -rf ${PACKAGE_PATH}
+        fi
+}
+
+package() {
+	cd ${OUTPUT_PATH}
+
+	if [ -d "${PACKAGE_PATH}" ]; then
+		echo "${PACKAGE_PATH} already exists."
+	fi
+
+	mkdir ${PACKAGE_PATH}
+	cd ${OUTPUT_PATH}
+	cp *.so ${PACKAGE_PATH}
+	#cp -r locales ${PACKAGE_PATH}
+	#cp nacl*.nexe ${PACKAGE_PATH}
+	#cp nacl_helper ${PACKAGE_PATH}
+	#cp nacl_helper_bootstrap ${PACKAGE_PATH}
+	cp *.pak ${PACKAGE_PATH}
+	cp ${BUILD_TARGET} ${PACKAGE_PATH}
+	tar -zcvf ${PACKAGE_FILE} ${PACKAGE_PATH}
+	rm -rf ${PACKAGE_PATH}
+	echo "> Complete: packaged files into ${PACKAGE_FILE}"
+}
+
+# do it
+clean
+package
